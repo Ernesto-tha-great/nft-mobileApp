def1aea39d9c9f4ec53658b49a0a666f1e13ce7c
Ernest Nnamdi <bulaveur@Ernests-MacBook-Pro.local>
2022-06-09 02:46:20 +0100

    first commit

---
+++ components/HomeHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, FlatList, SafeAreaView } from 'react-native'
+
+export const HomeHeader = () => {
+  return (
+    <View>
+      <Text>HomeHeader</Text>
+    </View>
+  )
+}
